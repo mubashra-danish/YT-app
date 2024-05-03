54f131cce18c27311a977065fa083a5186f24230
mubashra-danish <mubashradanish26@gmail.com>
2024-05-03 04:36:16 +0000

    Updated index.css and other files with styling changes and enhancements.

---
+++ src/components/TrendingCard/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './index.css'
+
+const TrendingCard = props => {
+  const {trendingData} = props
+  const {title, thumbnailUrl, channel, viewCount, publishedAt} = trendingData
+
+  return (
+    <li>
+      <Link to={`/videos/${id}`} className="link-item">
+        <div className="trending-card">
+          <img src={thumbnailUrl} alt={title} className="thumbnail" />
+          <div className="channel-info">
+            <h2 className="title">{title}</h2>
+            <p className="channel-name">{channel.name}</p>
+
+            <p className="view-count">{viewCount}</p>
+            <p className="published-at">{publishedAt}</p>
+          </div>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default TrendingCard
